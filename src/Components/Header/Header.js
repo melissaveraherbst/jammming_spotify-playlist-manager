994ed93c6b0373b21f4a543b02c69ed91e3f40f8
Clumsy_Cranberry_1 <mlsshrbst@gmail.com>
2024-01-15 21:58:20 +0200

    move files to appropriate folder locations

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 import './Header.css';
+import image from '../../assets/jammming_logo_logo.png'
 class Header extends React.Component {
     render() {
         const jsx = (
-
             <div className='Header'>
-                <img src="/images/jammming_logo_logo.png" alt="logo" />
+                <img src={image} alt="logo" />
             </div>
         );
         return jsx;
