8f34826e648c55423a8aafca52b3dbfe167137db
Clumsy_Cranberry_1 <mlsshrbst@gmail.com>
2024-01-15 14:23:08 +0200

    update ui colors

--- src/Components/ConnectToSpotify/Message/Message.js
+++ src/Components/ConnectToSpotify/Message/Message.js
@@ -1,12 +1,13 @@
 import React from "react";
+import './Message.css'
 
 class Message extends React.Component {
     render() {
         const jsx = (
-            <>
+            <div className="ConnectedStatus">
                 {this.props.isConnectedToSpotify ?
-                    <p>Connected</p> : <p>Not Connected</p>}
-            </>
+                    <p className="ConnectedStatusTrue">Connected</p> : <p className="ConnectedStatusFalse">Not Connected</p>}
+            </div>
         );
         return jsx;
     }
